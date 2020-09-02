8ee46c91a55e19f718eba96bb330b0dcd5b09e6e
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-09-02 22:51:57 +0200

    Fix

---
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using TheCodeCamp.Data;
+
+namespace TheCodeCamp.Controllers
+{
+    public class CampsController : ApiController
+    {
+        private readonly ICampRepository repository;
+
+        public CampsController(ICampRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<IHttpActionResult> Get()
+        {
+            try
+            {
+                var result = await repository.GetAllCampsAsync();
+
+                return Ok(result);
+            }
+            catch(Exception ex)
+            {
+                // TODO Add Logging
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
